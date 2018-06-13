082050cf9fa583a588d26288747aee40d3218535
NugeyBoom <nugebui@gmail.com>
2018-06-13 18:27:12 -0400

    add pom.xml and src files

---
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -0,0 +1,63 @@
+package com.dice;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DiceJobSearch {
+
+	public static void main(String[] args) {
+		//set up chrome driver path
+		WebDriverManager.chromedriver().setup();
+		//invoke selenium webdriver
+		WebDriver driver = new ChromeDriver();
+		//fullscreen
+		driver.manage().window().fullscreen();
+		//set universal wait time in case web page is slow
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//step 1. launch browser and navigate to https://dice.com
+		String url= "https://dice.com";
+		driver.get(url);
+		
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Job Search for Technology Professionals | Dice.com";
+		
+		if(actualTitle.equals(expectedTitle)) {
+			System.out.println("Step pass. Dice hompage successfully loaded");
+		} else {
+			System.out.println("Step FAIL. Dice homepage did not load successfully");
+			throw new RuntimeException("Step FAIL. Dice hompage did not load successfully");
+		}
+		
+		String keyword = "java developer";
+		driver.findElement(By.id("search-field-keyword")).clear();
+		driver.findElement(By.id("search-field-keyword")).sendKeys(keyword);
+		
+		String location = "20910";
+		
+		driver.findElement(By.id("search-field-location")).clear();
+		driver.findElement(By.id("search-field-location")).sendKeys(location);
+		
+		driver.findElement(By.id("findTechJobs")).click();
+		
+		String count = driver.findElement(By.id("posiCountId")).getText();
+		System.out.println(count);
+		
+		int countResult = Integer.parseInt(count.replace(",",""));
+		if (countResult > 0) {
+			System.out.println("Keyword: " + keyword + " Search returned " + countResult + " Results in "+ location);
+		} else {
+			System.out.println("Step Fail: keyword : " + keyword + " search returned" + countResult + " Results in "+ location);
+		}
+		
+		driver.close();
+				
+
+	}
+
+}
